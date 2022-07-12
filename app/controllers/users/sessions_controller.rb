54980e601c4ac03e7f715463fb8e7377e215d27c
Naoya0827 <n.freedom.yazuna@gmail.com>
2022-07-12 18:20:18 +0000

    [Finish]ユーザーアカウント停止機能追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,4 +24,14 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+  def after_sign_in_path_for(resource)
+    if resource.is_active == true
+      reset_session
+      new_user_session_path
+    else
+      root_path
+    end
+  end
 end
